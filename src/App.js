d8726fda2bb104ef180cabd5bf5b8db057e5c51c
faris-amer <farisamer569@gmail.com>
2024-04-15 07:02:59 -0400

    created react app, made basic header, made it so that svg icons would change color according to the theme of the website.

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.scss';
+import Linkedin from './icons.js';
+
+function App() {
+  return (
+    <>
+    
+    <div class='landingheader-container'>
+      <div class= 'left'>
+        <header>
+          Hello, I'm Faris.
+        </header>
+        <body>
+        Hello, I'm a second year mechatronics engineering student at Western University. I'm always working on new projects and love to learn things and stay busy throughout the year.  I have two passions: design and engineering, and you can browse some of my work below!
+        </body>
+      </div>
+      <div class="right">
+        <body>
+        farisamer569@gmail.com <br/>
+        (647) 580-9192
+        </body>
+        <Linkedin class="icon"/>
+      </div>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
